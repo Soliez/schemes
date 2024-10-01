f6a867222cb5a275dbf5a91b254022c9f24cbc8a
Erik Solis <solie2@icloud.com>
2024-10-01 18:18:18 -0400

    Refactored schemes.sh to ensure the value for CFBundleURLSchemes is extracted from each item in the array CFBundleURLTypes

--- schemes.sh
+++ schemes.sh
@@ -1,10 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 </path/to/Example.app>"
+
+ensure_trailing_slash() {
+    local path="$1"
+    if [[ "$path" != */ ]]; then
+        path="${path}/"
+    fi
+    echo "$path"
+}
+
+show_usage() {
+    printf 'Usage: %s </path/to/Example.app>' "$0"
     exit 1
-fi
+}
 
-path="$1/Contents/Info.plist"
 
-plutil -convert json "$path" -o - | jq .CFBundleURLTypes.[0].CFBundleURLSchemes
+main(){
+    if [[ -z "$1" ]]; then
+        show_usage
+    else
+        path="$1"
+        full_path="$(ensure_trailing_slash "$path")Contents/Info.plist"
+        plutil -convert json "$full_path" -o - | jq '.CFBundleURLTypes | map(.CFBundleURLSchemes)'
+    fi
+}
+
+main "$@"
